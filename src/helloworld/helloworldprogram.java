ce43678354fba2637f0060b7405705ca313f5f8c
unknown <sawarkarshivam18@gmail.com>
2021-12-26 11:17:46 +0530

    frist changes

---
+++ src/helloworld/helloworldprogram.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+public class helloworldprogram {  
+	
+	public void add()
+	{
+		System.out.println("add");
+	}
+	public int substract()
+	{
+		int c=a-b;
+		return c;
+	}
+	
+  public static void main( String args []) 
+  {
+	  helloworldprogram ab=new helloworldprogram ();
+	  ab.add();
+	  ab.substract();
+  }
+}
